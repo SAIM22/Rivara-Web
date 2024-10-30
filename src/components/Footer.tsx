3a389e770e1f5c9e59b96e0e596ecfa91c751a6b
Saim Rafiq <saimrafiq@Saims-MacBook-Pro.local>
2024-10-30 18:28:02 +0000

    CNAME added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Brain } from 'lucide-react';
 
 export default function Footer() {
@@ -39,8 +38,7 @@ export default function Footer() {
           <div>
             <h3 className="text-lg font-semibold mb-4">Contact</h3>
             <ul className="space-y-2 text-gray-400">
-              <li>info@rivara.consulting</li>
-              <li>+1 (555) 123-4567</li>
+              <li>ashadur@rivaraconsulting.co.uk</li>
             </ul>
           </div>
         </div>
